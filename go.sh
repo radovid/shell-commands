1f2d957557af04c8c5f28dbe55d114333c71c1f9
radovid <radovid@users.noreply.github.com>
2019-08-28 01:18:09 +0300

    Update go.sh
    * Don't overdo underlining
    * Rename sn variable to name

--- go.sh
+++ go.sh
@@ -2,40 +2,39 @@
 #### GO ####
 ############
 function go(){
-declare -- sn=$1;
+
+declare -- name=$1;
 declare -a found;
 declare -- idx=0;
 
 declare -- BO=$'\e[1m';
 declare -- BC=$'\e[22m';
-declare -- UO=$'\e[4m';
-declare -- UC=$'\e[24m';
 
-#if [[ $sn =~ trans|temp-|.*\.|.*\/.* ]] ;
+#if [[ $name =~ trans|temp-|.*\.|.*\/.* ]] ;
 
-if [ -d "$sn" ]; then 
-   echo "${BO}Going to ${UO}$sn${UC}${BC}";
-   cd "$sn";
+if [ -d "$name" ]; then 
+   echo "${BO}Going to $name${BC}";
+   cd "$name";
 
-elif [ -d /home/hermes/v2/"$sn" ]; then
-   echo "${BO}Going to ${UO}/home/hermes/v2/$sn${UC}${BC}";
-   cd /home/hermes/v2/"$sn";
+elif [ -d /home/hermes/v2/"$name" ]; then
+   echo "${BO}Going to /home/hermes/v2/$name${BC}";
+   cd /home/hermes/v2/"$name";
 
-elif [ -d /home/hermes/v2/gmi/"$sn" ]; then
-   echo "${BO}Going to ${UO}/home/hermes/v2/gmi/$sn${UC}${BC}";
-   cd /home/hermes/v2/gmi/"$sn";
+elif [ -d /home/hermes/v2/gmi/"$name" ]; then
+   echo "${BO}Going to /home/hermes/v2/gmi/$name${BC}";
+   cd /home/hermes/v2/gmi/"$name";
 
 else
 
   for dir in "." "/home/hermes/v2/gmi/v3/AMS" "/home/hermes/v2/gmi/v2" "/home/hermes/v2/bor/v1/AG" "/home/hermes/v2/lsr/bmr/v3" "/home/hermes/v2/lsr/bmr/v2" "/home/hermes/v2/gmi/v3/AMS/INTERNAL" "/home/hermes/v2/gmi" "/home/hermes/v2/gmi/v2/maps" "/home/hermes/v2/gmi/v3/AMS/NATO" "/home/hermes/v2/gmi/v2/NATO" ; do
-    if [ -d "$dir/$sn" ];
+    if [ -d "$dir/$name" ];
      then
-      found+=("$dir/$sn");
+      found+=("$dir/$name");
     fi
   done;
 
   if [ -z $found ] ; then
-    for line in $(find /home/hermes/v2 ! -readable -prune -o -name $sn -a -type d -print); do
+    for line in $(find /home/hermes/v2 ! -readable -prune -o -name $name -a -type d -print); do
       found+=("$line");
     done;
   fi;
@@ -44,16 +43,16 @@ else
     then
       for item in ${found[@]};  do
         idx=$((idx+1));
-        echo "${BO}$idx:${BC} ${UO}$item${UC}";
+        echo -e "${BO}$idx:${BC} \e[4m$item\e[24m";
       done;
       read -p "${BO}Enter the number of the directory to go to:${BC} " dirNum
 
-      echo "${BO}Going to ${UO}${found[$dirNum-1]}${UC}${BC}";
+      echo "${BO}Going to ${found[$dirNum-1]}${BC}";
       cd "${found[$dirNum-1]}";
 
    elif [ ${#found[@]} -eq 1 ];
     then
-      echo "${BO}Going to ${UO}${found[0]}${UC}${BC}";
+      echo "${BO}Going to ${found[0]}${BC}";
       cd "${found[0]}";
  
     else
@@ -63,4 +62,5 @@ else
    fi;
 
 fi;
+
 };
