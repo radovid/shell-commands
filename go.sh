e35ca16e9a0de127978d7c534d7e5473f9c2a5ce
radovid <radovid@users.noreply.github.com>
2019-08-28 01:04:58 +0300

    Several updates
    * Always check for a directory, don't ignore temps and paths with /
    * Added text formatting for better readability
    * Command structure updated

--- go.sh
+++ go.sh
@@ -3,68 +3,64 @@
 ############
 function go(){
 declare -- sn=$1;
+declare -a found;
+declare -- idx=0;
 
+declare -- BO=$'\e[1m';
+declare -- BC=$'\e[22m';
+declare -- UO=$'\e[4m';
+declare -- UC=$'\e[24m';
 
-if [[ $sn =~ trans|temp-|.*\.|.*\/.* ]] ;
- then
-   if [ -d "$sn" ]; then 
-     cd "$sn";
-   elif [ -d /home/hermes/v2/"$sn" ]; then
-     cd /home/hermes/v2/"$sn";
-   elif [ -d /home/hermes/v2/gmi/"$sn" ]; then
-     cd /home/hermes/v2/gmi/"$sn";
-   else
-     echo "Sorry, didn't find where to go!"
-     return 1
-   fi
+#if [[ $sn =~ trans|temp-|.*\.|.*\/.* ]] ;
+
+if [ -d "$sn" ]; then 
+   echo "${BO}Going to ${UO}$sn${UC}${BC}";
+   cd "$sn";
+
+elif [ -d /home/hermes/v2/"$sn" ]; then
+   echo "${BO}Going to ${UO}/home/hermes/v2/$sn${UC}${BC}";
+   cd /home/hermes/v2/"$sn";
+
+elif [ -d /home/hermes/v2/gmi/"$sn" ]; then
+   echo "${BO}Going to ${UO}/home/hermes/v2/gmi/$sn${UC}${BC}";
+   cd /home/hermes/v2/gmi/"$sn";
+
 else
 
- declare -a found;
- for dir in "/home/hermes/v2/gmi/v3/AMS" "/home/hermes/v2/gmi/v2" "/home/hermes/v2/bor/v1/AG" "/home/hermes/v2/lsr/bmr/v3" "/home/hermes/v2/lsr/bmr/v2" "/home/hermes/v2/gmi/v3/AMS/INTERNAL" "/home/hermes/v2/gmi" "/home/hermes/v2/gmi/v2/maps" "/home/hermes/v2/gmi/v3/AMS/NATO" "/home/hermes/v2/gmi/v2/NATO" ; do
-  if [ -d "$dir/$sn" ];
-   then
-    found+=("$dir/$sn");
-  fi
- done;
+  for dir in "." "/home/hermes/v2/gmi/v3/AMS" "/home/hermes/v2/gmi/v2" "/home/hermes/v2/bor/v1/AG" "/home/hermes/v2/lsr/bmr/v3" "/home/hermes/v2/lsr/bmr/v2" "/home/hermes/v2/gmi/v3/AMS/INTERNAL" "/home/hermes/v2/gmi" "/home/hermes/v2/gmi/v2/maps" "/home/hermes/v2/gmi/v3/AMS/NATO" "/home/hermes/v2/gmi/v2/NATO" ; do
+    if [ -d "$dir/$sn" ];
+     then
+      found+=("$dir/$sn");
+    fi
+  done;
 
- declare -- idx=0;
- if [ ${#found[@]} -gt 1 ];
-  then
-   for item in ${found[@]};  do
-     idx=$((idx+1));
-     echo $idx': ' $item;
-   done;
-    read -p "Enter the number of the directory to go to: " dirNum
-    echo "${found[$dirNum-1]}";
-    cd "${found[$dirNum-1]}";
- elif [ ${#found[@]} -eq 1 ];
-  then
-    echo "${found[0]}";
-    cd "${found[0]}";
- else
-   declare -a foundm;
-   for line in $(find /home/hermes/v2 ! -readable -prune -o -name $sn -a -type d -print); do
-     foundm+=("$line");
-   done;
+  if [ -z $found ] ; then
+    for line in $(find /home/hermes/v2 ! -readable -prune -o -name $sn -a -type d -print); do
+      found+=("$line");
+    done;
+  fi;
 
-   declare -- idm=0;
-   if [ ${#foundm[@]} -gt 1 ];
+  if [ ${#found[@]} -gt 1 ];
     then
-     for item in ${foundm[@]};  do
-       idm=$((idm+1));
-       echo $idm': ' $item;
-     done;
-      read -p "Enter the number of the directory to go to: " dirNum
-      echo "${foundm[$dirNum-1]}";
-      cd "${foundm[$dirNum-1]}";
-   elif [ ${#foundm[@]} -eq 1 ];
+      for item in ${found[@]};  do
+        idx=$((idx+1));
+        echo "${BO}$idx:${BC} ${UO}$item${UC}";
+      done;
+      read -p "${BO}Enter the number of the directory to go to:${BC} " dirNum
+
+      echo "${BO}Going to ${UO}${found[$dirNum-1]}${UC}${BC}";
+      cd "${found[$dirNum-1]}";
+
+   elif [ ${#found[@]} -eq 1 ];
     then
-      echo "${foundm[0]}";
-      cd "${foundm[0]}";
-      
+      echo "${BO}Going to ${UO}${found[0]}${UC}${BC}";
+      cd "${found[0]}";
+ 
+    else
+      echo -e "\e[38;2;240;143;104mSorry, didn't find where to go!\e[39m"
+      return 1
+ 
    fi;
-
- fi;
 
 fi;
 };
